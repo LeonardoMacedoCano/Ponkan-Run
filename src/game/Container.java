107c5c22c3380b3d60df7c97db0eb14cff118c2d
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-07-23 13:46:20 -0300

    Change class container to open game window

--- src/game/Container.java
+++ src/game/Container.java
@@ -1,8 +1,22 @@
 package game;
 
-public class Container {
+import javax.swing.*;
+
+public class Container extends JFrame {
+    private final String TITLE = "Ponkan Run";
+    private final int HEIGHT = 765;
+    private final int WIDTH = 1040;
+
+    private Container() {
+        setTitle(TITLE);
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Container();
     }
 }
