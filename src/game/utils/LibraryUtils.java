ffec17a9ae37d77a7aed6902c63468316ae589a9
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-07-28 00:16:13 -0300

    Add LibraryUtils class

---
+++ src/game/utils/LibraryUtils.java
@@ -0,0 +1,7 @@
+package game.utils;
+
+public class LibraryUtils {
+    public static final String PATH_IMG = "src/game/image/";
+    public static final String PATH_IMG_STAGE_BG = String.format("%s/stage-background/", PATH_IMG);
+
+}
