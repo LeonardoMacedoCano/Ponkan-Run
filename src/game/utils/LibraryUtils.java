65df16e8d7796e4a246b19f3d5dd7a94dbc21903
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-07-30 01:58:08 -0300

    Add path to player-stopped images

--- src/game/utils/LibraryUtils.java
+++ src/game/utils/LibraryUtils.java
@@ -1,7 +1,10 @@
 package game.utils;
 
 public class LibraryUtils {
-    public static final String PATH_IMG = "src/game/image/";
+    private static final String PATH_IMG = "src/game/image/";
+    private static final String PATH_IMG_PLAYER = String.format("%s/player/", PATH_IMG);
+
     public static final String PATH_IMG_STAGE_BG = String.format("%s/stage-background/", PATH_IMG);
+    public static final String PATH_IMG_PLAYER_STOPPED = String.format("%s/player-stopped/", PATH_IMG_PLAYER);
 
 }
