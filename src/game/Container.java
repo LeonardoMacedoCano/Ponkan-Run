5e359f3b4e699256087977df796e89da679c012a
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-07-30 14:52:12 -0300

    Add image icon to container class

--- src/game/Container.java
+++ src/game/Container.java
@@ -1,15 +1,22 @@
 package game;
 
+import game.utils.LibraryUtils;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Container extends JFrame {
     private final String TITLE = "Ponkan Run";
     private final int DEFAULT_HEIGHT = 765;
     private final int DEFAULT_WIDTH = 1040;
+    private final String IMAGE_ICON = String.format("%s/icon.png", LibraryUtils.PATH_IMG_PLAYER);
 
     private Container() {
+        ImageIcon icon = new ImageIcon(IMAGE_ICON);
+
         add(new Stage());
         setTitle(TITLE);
+        setIconImage(icon.getImage());
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
--- src/game/utils/LibraryUtils.java
+++ src/game/utils/LibraryUtils.java
@@ -2,8 +2,7 @@ package game.utils;
 
 public class LibraryUtils {
     private static final String PATH_IMG = "src/game/image/";
-    private static final String PATH_IMG_PLAYER = String.format("%s/player/", PATH_IMG);
-
+    public static final String PATH_IMG_PLAYER = String.format("%s/player/", PATH_IMG);
     public static final String PATH_IMG_STAGE_BG = String.format("%s/stage-background/", PATH_IMG);
     public static final String PATH_IMG_PLAYER_STOPPED = String.format("%s/player-stopped/", PATH_IMG_PLAYER);
 
