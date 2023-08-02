b74f806a6d908b5c52e869f2c7d4d2761c7fd356
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-08-01 23:11:51 -0300

    Add ObstacleType class

---
+++ src/game/utils/ObstacleType.java
@@ -0,0 +1,25 @@
+package game.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObstacleType {
+    public static final String CUP = "Cup";
+    public static final String TEA_CUP = "Tea cup";
+    public static final String KNIFE = "Knife";
+
+    private static final List<String> obstacleType = new ArrayList<>();
+    static {
+        obstacleType.add(CUP);
+        obstacleType.add(TEA_CUP);
+        obstacleType.add(KNIFE);
+    }
+
+    public static List<String> getObstacleType() {
+        return obstacleType;
+    }
+
+    public static int getTotalObstacleType() {
+        return obstacleType.size();
+    }
+}
